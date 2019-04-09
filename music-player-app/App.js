09087a89f80f66b7539b5a208f5b9cb060640019
chern0us <belousnikita@gmail.com>
2019-04-09 19:46:18 +0300

    prettier

--- music-player-app/App.js
+++ music-player-app/App.js
@@ -17,39 +17,38 @@ export default class App extends React.Component {
     super(props);
   }
   render() {
-  return (
-    <Router>
-      <Scene key="root">
-        {/* Tab Container */}
-        <Scene
-          key="tabbar"
-          tabs={true}
-          hideNavBar={true}
-          tabBarStyle={{ backgroundColor: "#FFFFFF" }}
-        >
-          {/* Main tab*/}
-          <Scene key="one" title="1" icon={TabIcon}>
-            <Scene key="main" component={MainScreen} title="Title" />
-            <Scene key="player" component={PlayerScreen} title="Song" />
-          </Scene>
+    return (
+      <Router>
+        <Scene key="root">
+          {/* Tab Container */}
+          <Scene
+            key="tabbar"
+            tabs={true}
+            hideNavBar={true}
+            tabBarStyle={{ backgroundColor: "#FFFFFF" }}
+          >
+            {/* Main tab*/}
+            <Scene key="one" title="1" icon={TabIcon}>
+              <Scene key="main" component={MainScreen} title="Title" />
+              <Scene key="player" component={PlayerScreen} title="Song" />
+            </Scene>
 
-          {/* Empty scene*/}
-          <Scene key="two" title="2" icon={TabIcon}>
-            <Scene key="empty" component={EmptyComponent} title="Title" />
-          </Scene>
+            {/* Empty scene*/}
+            <Scene key="two" title="2" icon={TabIcon}>
+              <Scene key="empty" component={EmptyComponent} title="Title" />
+            </Scene>
 
-          {/* Empty scene*/}
-          <Scene key="three" title="3" icon={TabIcon}>
-            <Scene key="empty" component={EmptyComponent} title="Title" />
-          </Scene>
-          {/* Empty scene*/}
-          <Scene key="four" title="4" icon={TabIcon}>
-            <Scene key="empty" component={EmptyComponent} title="Title" />
+            {/* Empty scene*/}
+            <Scene key="three" title="3" icon={TabIcon}>
+              <Scene key="empty" component={EmptyComponent} title="Title" />
+            </Scene>
+            {/* Empty scene*/}
+            <Scene key="four" title="4" icon={TabIcon}>
+              <Scene key="empty" component={EmptyComponent} title="Title" />
+            </Scene>
           </Scene>
         </Scene>
-      </Scene>
-    </Router>
-  );
+      </Router>
+    );
   }
-};
-
+}
