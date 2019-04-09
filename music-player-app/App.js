aafcf1134027d19ee877e5eb4827cbcca3f824fe
chern0us <belousnikita@gmail.com>
2019-04-09 19:44:37 +0300

    basic screen division and nav tabs

--- music-player-app/App.js
+++ music-player-app/App.js
@@ -1,21 +1,55 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import { Text } from "react-native";
+import { Router, Scene } from "react-native-router-flux";
+
+// New Imports
+
+import MainScreen from "./components/MainScreen";
+import PlayerScreen from "./components/PlayerScreen";
+import EmptyComponent from "./components/EmptyComponent";
+// Simple component to render something in place of icon
+const TabIcon = ({ selected, title }) => {
+  return <Text style={{ color: selected ? "red" : "black" }}>{title}</Text>;
+};
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
+  constructor(props) {
+    super(props);
   }
-}
+  render() {
+  return (
+    <Router>
+      <Scene key="root">
+        {/* Tab Container */}
+        <Scene
+          key="tabbar"
+          tabs={true}
+          hideNavBar={true}
+          tabBarStyle={{ backgroundColor: "#FFFFFF" }}
+        >
+          {/* Main tab*/}
+          <Scene key="one" title="1" icon={TabIcon}>
+            <Scene key="main" component={MainScreen} title="Title" />
+            <Scene key="player" component={PlayerScreen} title="Song" />
+          </Scene>
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+          {/* Empty scene*/}
+          <Scene key="two" title="2" icon={TabIcon}>
+            <Scene key="empty" component={EmptyComponent} title="Title" />
+          </Scene>
+
+          {/* Empty scene*/}
+          <Scene key="three" title="3" icon={TabIcon}>
+            <Scene key="empty" component={EmptyComponent} title="Title" />
+          </Scene>
+          {/* Empty scene*/}
+          <Scene key="four" title="4" icon={TabIcon}>
+            <Scene key="empty" component={EmptyComponent} title="Title" />
+          </Scene>
+        </Scene>
+      </Scene>
+    </Router>
+  );
+  }
+};
+
