03b25ea8dac481879ec29d61162dc379cd0d353f
chern0us <belousnikita@gmail.com>
2019-04-09 19:47:15 +0300

    player screen

---
+++ music-player-app/components/PlayerScreen.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+
+const PlayerScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.welcome}>
+        This is player
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#bb0000',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+    color: '#ffffff',
+  },
+});
+
+export default PlayerScreen;
