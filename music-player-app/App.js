025c90dfa0ce5efb30f4c501ad7c3e553571fc6a
chern0us <belousnikita@gmail.com>
2019-04-10 15:27:11 +0300

    PlayList and PlayList Item

--- music-player-app/App.js
+++ music-player-app/App.js
@@ -1,38 +1,86 @@
-import React, { Component } from "react";
+import React from "react";
 import { Text } from "react-native";
-import { Router, Scene } from "react-native-router-flux";
-
-// New Imports
-
+import { Router, Scene, Stack } from "react-native-router-flux";
 import MainScreen from "./components/MainScreen";
 import PlayerScreen from "./components/PlayerScreen";
 import EmptyComponent from "./components/EmptyComponent";
-// Simple component to render something in place of icon
-const TabIcon = ({ title }) => {
-  return <Text style={{ fontSize: 20}}>{title}</Text>;
-};
 
+const TabIcon = ({ title }) => {
+  return <Text style={{ fontSize: 20 }}>{title}</Text>;
+};
+class PlaylistItem {
+  constructor(name, uri, image) {
+    this.name = name;
+    this.uri = uri;
+    this.image = image;
+  }
+}
 export default class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      playList: [
+        new PlaylistItem(
+          "Comfort Fit - “Sorry”",
+          "https://s3.amazonaws.com/exp-us-standard/audio/playlist-example/Comfort_Fit_-_03_-_Sorry.mp3",
+          "http://www.sclance.com/pngs/vinyl-records-png/vinyl_records_png_1469170.jpg"
+        ),
+        new PlaylistItem(
+          "Mildred Bailey – “All Of Me”",
+          "https://ia800304.us.archive.org/34/items/PaulWhitemanwithMildredBailey/PaulWhitemanwithMildredBailey-AllofMe.mp3",
+          "http://www.sclance.com/pngs/vinyl-records-png/vinyl_records_png_1468942.png"
+        ),
+        new PlaylistItem(
+          "Podington Bear - “Rubber Robot”",
+          "https://s3.amazonaws.com/exp-us-standard/audio/playlist-example/Podington_Bear_-_Rubber_Robot.mp3",
+          "http://www.sclance.com/pngs/vinyl-records-png/vinyl_records_png_1469228.jpg"
+        ),
+        new PlaylistItem(
+          'Feather Duster- "Geographer"',
+          "https://a.uguu.se/W85gJqmRhaCV_Feather_Duster.mp3",
+          "http://www.sclance.com/pngs/vinyl-records-png/vinyl_records_png_1469170.jpg"
+        ),
+        new PlaylistItem(
+          'Anxious- "Anxious"',
+          "https://ia800304.us.archive.org/34/items/PaulWhitemanwithMildredBailey/PaulWhitemanwithMildredBailey-AllofMe.mp3",
+          "http://www.sclance.com/pngs/vinyl-records-png/vinyl_records_png_1469170.jpg"
+        ),
+        new PlaylistItem(
+          "Track - “Song”",
+          "https://s3.amazonaws.com/exp-us-standard/audio/playlist-example/Podington_Bear_-_Rubber_Robot.mp3",
+          "http://www.sclance.com/pngs/vinyl-records-png/vinyl_records_png_1468942.png"
+        )
+      ]
+    };
   }
   render() {
+    const { playList } = this.state;
+    // Actions.refresh({key: "player", title});
     return (
       <Router>
-        <Scene key="root">
+        <Stack key="root">
           {/* Tab Container */}
           <Scene
             key="tabbar"
             tabs={true}
             hideNavBar={true}
-            tabBarStyle={{ backgroundColor: "#FFFFFF"}}
-            labelStyle={{display: 'none'}}
-            activeBackgroundColor="#c0e2dc"
+            tabBarStyle={{ backgroundColor: "#FFFFFF" }}
+            labelStyle={{ display: "none" }}
+            activeBackgroundColor="#56D5FA"
           >
             {/* Main tab*/}
             <Scene key="one" title="1" icon={TabIcon}>
-              <Scene key="main" component={MainScreen} title="Title" />
-              <Scene key="player" component={PlayerScreen} title="Song" />
+              <Scene
+                key="main"
+                playList={playList}
+                component={MainScreen}
+                title="Title"
+              />
+              <Scene
+                key="player"
+                component={PlayerScreen}
+                playList={playList}
+              />
             </Scene>
 
             {/* Empty scene*/}
@@ -49,7 +97,7 @@ export default class App extends React.Component {
               <Scene key="empty" component={EmptyComponent} title="Title" />
             </Scene>
           </Scene>
-        </Scene>
+        </Stack>
       </Router>
     );
   }
