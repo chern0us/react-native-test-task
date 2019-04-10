4315cd8fe4fb93418a98877b1376ff2524a3d479
chern0us <belousnikita@gmail.com>
2019-04-10 15:27:59 +0300

    Playlist and track selection

--- music-player-app/components/MainScreen.js
+++ music-player-app/components/MainScreen.js
@@ -1,15 +1,45 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  TouchableHighlight
+} from "react-native";
 import { Actions } from "react-native-router-flux";
-const MainScreen = () => {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.welcome} onPress={() => Actions.player()}>
-        This is the song name
-      </Text>
-    </View>
-  );
-};
+import { Audio } from 'expo';
+
+export default class MainScreen extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <FlatList
+          style={styles.trackList}
+          data={this.props.playList}
+          keyExtractor={(item, index) => index.toString()}
+          renderItem={({ item, index }) => {
+            return (
+              <TouchableHighlight
+                style={styles.trackListItem}
+                onPress={() => {
+                  Actions.player({index,  title: item.name});
+                  Audio.setIsEnabledAsync(false);
+                  Audio.setIsEnabledAsync(true);
+                }}
+                underlayColor="#56D5FA"
+              >
+                <Text key={item.id} style={styles.trackListItemText}>{item.name}</Text>
+              </TouchableHighlight>
+            );
+          }}
+        />
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -22,7 +52,20 @@ const styles = StyleSheet.create({
     fontSize: 20,
     textAlign: "center",
     margin: 10
+  },
+  trackList: {
+    width: "100%"
+  },
+  trackListItem: {
+    paddingBottom: 20,
+    paddingTop: 20,
+    paddingLeft: 15,
+    width: "100%",
+    borderBottomWidth: 1,
+    borderBottomColor: "black"
+  },
+  trackListItemText: {
+    color: "black",
+    fontSize: 16
   }
 });
-
-export default MainScreen;
